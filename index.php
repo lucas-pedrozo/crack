dd181e5e632520fec34a1eacaa91d25d0c370d0d
lucas-pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2024-05-17 02:27:08 -0300

    versão 1.0

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    
+    <title>Crack Games</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+</head>
+<body> 
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary"  id="navbar" data-aos="fade-down" data-aos-duration="1000ms">
+
+    <div class="container-fluid">
+
+        <a class="navbar-brand" href="#">
+    <img src="imagens/logo.png" alt=" Crack games" title="crack games">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="menu navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="contato">CONTATO</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                JOGOS
+            </a>
+            <ul class="dropdown-menu ">
+                <li><a class="dropdown-item" href="golden.php">Em Busca Da Golden Apple</a></li>
+                <li><a class="dropdown-item" href="index.php">Em Busca Da Golden Apple</a></li>
+                <li><a class="dropdown-item" href="#">Em Busca Da Golden Apple</a></li>
+            </ul>
+            </li>
+        </ul>
+       <a href="#" class="btn btn-warning">
+        <strong>LOGIN</strong>
+       </a>
+        </div>
+    </div>
+</nav>
+
+
+<main>
+        
+    <?php
+
+    if (isset($_GET["param"])) {
+        $param = $_GET["param"];  
+        $p = explode("/", $param);
+    }
+
+    $page = $p[0] ?? "home" ;
+    $jogo = $p[1] ?? NULL;
+
+    if ($page == "jogo") {
+        $pagina = "jogo/{$jogo}.php";
+    } else {
+        $pagina = "paginas/{$page}.php";
+    }
+
+    if (file_exists($pagina)) {
+        include $pagina;
+    } else {
+        include "paginas/erro.php";
+    }
+
+    ?>
+    </main>
+
+    <footer class="footer">
+        <h2>Contato</h2>
+        <div class="links">
+        <a href="#">
+            <i class="bi bi-instagram"></i>
+        </a>
+        <a href="">
+        <i class="bi bi-github"></i>
+        </a>
+        <a href="">
+        <i class="bi bi-whatsapp"></i>
+        </a>
+        </div>
+    </footer>
+
+    <!--codigos javascript-->
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <script>
+        AOS.init();
+    </script>
+
+</body>
+</html>
---
+++ paginas/home.php
@@ -0,0 +1,62 @@
+
+<div id="carouselExampleRide" class="carousel slide" data-bs-ride="true" data-aos="fade-up" data-aos-duration="800ms">
+<div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imagens/banner-1.png" class="d-block w-100" alt="Em busca da golden apple">
+    </div>
+    <div class="carousel-item">
+      <img src="imagens/banner-1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imagens/banner-1.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<h2 class="text-center margin0" data-aos="fade-right" data-aos-duration="800ms">Em Destaque</h2>
+
+
+<div class="cart">
+
+    <div class="cart-block" data-aos="fade-up" data-aos-duration="800ms">
+        <a href="#">
+            <img src="imagens/capa.png" alt="Clique para Jogar" title="Clique para Jogar">
+        </a>
+           <div class="descricao">
+           <h3 class="cart-center">Em Busca Da Golden Apple</h3>
+           <h3 class="cart-center"><span>R$:99,00</span></h3>
+           </div>
+    </div>
+    <div class="cart-block" data-aos="fade-up" data-aos-duration="800ms">
+        <a href="#">
+            <img src="imagens/capa.png" alt="Clique para Jogar" title="Clique para Jogar">
+        </a>
+           <div class="descricao">
+           <h3 class="cart-center">Em Busca Da Golden Apple</h3>
+           <h3 class="cart-center"><span>R$:99,00</span></h3>
+           </div>
+    </div>
+    <div class="cart-block" data-aos="fade-up" data-aos-duration="800ms">
+        <a href="#">
+            <img src="imagens/capa.png" alt="Clique para Jogar" title="Clique para Jogar">
+        </a>
+           <div class="descricao">
+           <h3 class="cart-center">Em Busca Da Golden Apple</h3>
+           <h3 class="cart-center"><span>R$:99,00</span></h3>
+           </div>
+    </div>
+
+</div>
